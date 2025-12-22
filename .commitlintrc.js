1396edfa85a1fa5f121626b7a7c9d1b9f25c5da5
weizhifeng <18797942292@139.com>
2025-12-22 16:36:31 +0800

    feat: 提升检查效率

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -28,12 +28,12 @@ module.exports = {
 
 // 第二步 启用 Husky
 /*
-* # 初始化 husky
-* npx husky install
-* 
-* # 将 husky 安装脚本添加到 package.json，确保团队成员安装依赖后自动启用
-* npm pkg set scripts.prepare="husky install"
-*/
+ * # 初始化 husky
+ * npx husky install
+ *
+ * # 将 husky 安装脚本添加到 package.json，确保团队成员安装依赖后自动启用
+ * npm pkg set scripts.prepare="husky install"
+ */
 
 // 第三步 配置校验规则
 /**
@@ -66,3 +66,27 @@ module.exports = {
 // 第四步：添加 Git Hook
 
 // npx husky add .husky/commit-msg 'npx --no -- commitlint --edit ${1}'
+
+/**
+ * 修复husky不生效
+ * 1. 彻底重置 Husky npx husky init
+ * 2.在.husky/commit-msg新建commit-msg添加npx --no -- commitlint --edit $1
+ * 3. git add .husky/commit-msg
+ * 4.git update-index --chmod=+x .husky/commit-msg
+ */
+
+/**
+ * 提升速度
+ * 1.安装依赖 pnpm install -D lint-staged eslint -w
+ * 2.在package.json 添加
+ * "lint-staged": {
+    "*.{js,ts,vue}": [
+      "eslint --fix",
+      "prettier --write"
+    ],
+    "*.{css,less,scss}": [
+      "prettier --write"
+    ]
+  }
+    3. 在.husky/pre-commit里面添加 npx lint-staged
+ */
